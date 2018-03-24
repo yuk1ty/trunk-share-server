e8fac5f7a740b38f7d5d3896431e55251874d977
yuk1ty <toyoda_yuki@cyberagent.co.jp>
2018-03-24 13:59:46 +0900

    Add some experimental endpoints

--- src/main/scala/route/driver/DriversEndpoint.scala
+++ src/main/scala/route/driver/DriversEndpoint.scala
@@ -1,15 +1,44 @@
 package route.driver
 
 import com.twitter.finagle.http.Status
-import domain.Driver
+import domain.{ByMotorcycle, Driver, Weekdays}
+import io.finch._
 import io.finch.syntax._
 import io.finch.{Endpoint, Output}
 import usecase.{MixInDriverUsecase, UsesDriverUsecase}
 
 trait DriversEndpoint extends UsesDriverUsecase {
 
-  def apply(): Endpoint[Seq[Driver]] = get("drivers") {
-    Output.payload(Seq(Driver(1L, "Tokyo", "Saitama", 3)), Status.Accepted)
+  def apply() = all() :+: create()
+
+  def all(): Endpoint[Seq[Driver]] = get("drivers") {
+    Output.payload(
+      Seq(
+        Driver(id = 1,
+               whereFrom = "Saitama",
+               whereTo = "Tokyo",
+               startAt = "08:00",
+               endAt = "09:00",
+               rating = 1,
+               commutionType = ByMotorcycle,
+               frequency = Weekdays)),
+      Status.Accepted
+    )
+  }
+
+  def create(): Endpoint[Seq[Driver]] = get("drivers" :: "new") {
+    Output.payload(
+      Seq(
+        Driver(id = 1,
+               whereFrom = "Saitama",
+               whereTo = "Tokyo",
+               startAt = "08:00",
+               endAt = "09:00",
+               rating = 1,
+               commutionType = ByMotorcycle,
+               frequency = Weekdays)),
+      Status.Accepted
+    )
   }
 }
 
