248e3a7e94c1e3858d6684692b02eac673b8574d
yuk1ty <toyoda_yuki@cyberagent.co.jp>
2018-03-25 12:35:37 +0900

    Modify usecase

--- src/main/scala/route/driver/DriversEndpoint.scala
+++ src/main/scala/route/driver/DriversEndpoint.scala
@@ -16,7 +16,7 @@ trait DriversEndpoint
     with UsesDriverRepository
     with LazyLogging {
 
-  def apply() = all() :+: create() :+: update() :+: mock()
+  def apply() = all() :+: create() :+: update() :+: mock() :+: createOp()
 
   def mock(): Endpoint[Seq[Driver]] = get("drivers" :: "mock") {
     Output.payload(
@@ -63,11 +63,20 @@ trait DriversEndpoint
     post("drivers" :: "new" :: jsonBody[Driver]) { driver: Driver =>
       driverRepository.save(driver)
       println(s"保存に成功しました: $driver")
-      Ok(driver)
+      Output
+        .payload(driver, Status.Accepted)
         .withHeader(("Access-Control-Allow-Origin", "*"))
-        .withHeader(("Access-Control-Allow-Methods", "GET,POST,HEAD,OPTIONS"))
+        .withHeader(("Access-Control-Allow-Methods", "POST,HEAD,OPTIONS"))
         .withHeader(("Access-Control-Allow-Credentials", "true"))
+        .withHeader(
+          ("Access-Control-Allow-Headers",
+           "Accept, Accept-CH, Accept-Charset, Accept-Datetime, Accept-Encoding, Accept-Ext, Accept-Features, Accept-Language, Accept-Params, Accept-Ranges, Access-Control-Allow-Credentials, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Expose-Headers, Access-Control-Max-Age, Access-Control-Request-Headers, Access-Control-Request-Method, Age, Allow, Alternates, Authentication-Info, Authorization, C-Ext, C-Man, C-Opt, C-PEP, C-PEP-Info, CONNECT, Cache-Control, Compliance, Connection, Content-Base, Content-Disposition, Content-Encoding, Content-ID, Content-Language, Content-Length, Content-Location, Content-MD5, Content-Range, Content-Script-Type, Content-Security-Policy, Content-Style-Type, Content-Transfer-Encoding, Content-Type, Content-Version, Cookie, Cost, DAV, DELETE, DNT, DPR, Date, Default-Style, Delta-Base, Depth, Derived-From, Destination, Differential-ID, Digest, ETag, Expect, Expires, Ext, From, GET, GetProfile, HEAD, HTTP-date, Host, IM, If, If-Match, If-Modified-Since, If-None-Match, If-Range, If-Unmodified-Since, Keep-Alive, Label, Last-Event-ID, Last-Modified, Link, Location, Lock-Token, MIME-Version, Man, Max-Forwards, Media-Range, Message-ID, Meter, Negotiate, Non-Compliance, OPTION, OPTIONS, OWS, Opt, Optional, Ordering-Type, Origin, Overwrite, P3P, PEP, PICS-Label, POST, PUT, Pep-Info, Permanent, Position, Pragma, ProfileObject, Protocol, Protocol-Query, Protocol-Request, Proxy-Authenticate, Proxy-Authentication-Info, Proxy-Authorization, Proxy-Features, Proxy-Instruction, Public, RWS, Range, Referer, Refresh, Resolution-Hint, Resolver-Location, Retry-After, Safe, Sec-Websocket-Extensions, Sec-Websocket-Key, Sec-Websocket-Origin, Sec-Websocket-Protocol, Sec-Websocket-Version, Security-Scheme, Server, Set-Cookie, Set-Cookie2, SetProfile, SoapAction, Status, Status-URI, Strict-Transport-Security, SubOK, Subst, Surrogate-Capability, Surrogate-Control, TCN, TE, TRACE, Timeout, Title, Trailer, Transfer-Encoding, UA-Color, UA-Media, UA-Pixels, UA-Resolution, UA-Windowpixels, URI, Upgrade, User-Agent, Variant-Vary, Vary, Version, Via, Viewport-Width, WWW-Authenticate, Want-Digest, Warning, Width, X-Content-Duration, X-Content-Security-Policy, X-Content-Type-Options, X-CustomHeader, X-DNSPrefetch-Control, X-Forwarded-For, X-Forwarded-Port, X-Forwarded-Proto, X-Frame-Options, X-Modified, X-OTHER, X-PING, X-PINGOTHER, X-Powered-By, X-Requested-With"))
+        .withHeader(("Content-Type", "application/json; charset=utf8"))
     }
+
+  def createOp(): Endpoint[String] = options("drivers" :: "new") {
+    Ok("OK")
+  }
 
   def update(): Endpoint[Driver] =
     patch("drivers" :: "rating" :: "update" :: jsonBody[DriverRating]) {
@@ -80,14 +89,17 @@ trait DriversEndpoint
             Ok(update)
               .withHeader(("Access-Control-Allow-Origin", "*"))
               .withHeader(
-                ("Access-Control-Allow-Methods", "GET,POST,HEAD,OPTIONS"))
+                ("Access-Control-Allow-Methods", "POST,PATCH,HEAD,OPTIONS"))
+              .withHeader(("Access-Control-Allow-Credentials", "true"))
+              .withHeader(("Access-Control-Allow-Headers", "*"))
           case None => {
             println("ID に該当するドライバーがいませんでした．")
             NoContent
               .withHeader(("Access-Control-Allow-Origin", "*"))
               .withHeader(
-                ("Access-Control-Allow-Methods", "GET,POST,HEAD,OPTIONS"))
+                ("Access-Control-Allow-Methods", "POST,PATCH,HEAD,OPTIONS"))
               .withHeader(("Access-Control-Allow-Credentials", "true"))
+              .withHeader(("Access-Control-Allow-Headers", "*"))
           }
         }
     }
--- src/main/scala/usecase/DriverUsecase.scala
+++ src/main/scala/usecase/DriverUsecase.scala
@@ -4,13 +4,13 @@ import domain.{Driver, DriverRating}
 
 trait DriverUsecase {
   def updateDriverRating(rating: DriverRating, driver: Driver): Driver = {
-    val driverRate = if (rating.safety < 10) {
+    val driverRate = if (rating.safety < 30) {
       5
-    } else if (rating.safety >= 10 && rating.safety < 20) {
+    } else if (rating.safety >= 30 && rating.safety < 70) {
       4
-    } else if (rating.safety >= 20 && rating.safety < 30) {
+    } else if (rating.safety >= 70 && rating.safety < 110) {
       3
-    } else if (rating.safety >= 30 && rating.safety < 40) {
+    } else if (rating.safety >= 150 && rating.safety < 190) {
       2
     } else {
       1
